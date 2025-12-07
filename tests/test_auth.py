41e5fbc6c5beb0cbfdd50c662ce32c8de3e2fc82
Дарья Федина <darya.fedina.05@mail.ru>
2025-12-08 00:48:22 +0300

    Fix PEP8 issues in urls.py, complete Yatube API with all tests passing

---
+++ tests/test_auth.py
@@ -0,0 +1,25 @@
+from http import HTTPStatus
+
+import pytest
+
+
+class TestAuthAPI:
+    
+    def test_auth(self, client):
+        """Тест существования эндпоинта аутентификации."""
+        url = '/api/v1/api-token-auth/'
+        response = client.post(url, data={})
+        
+        assert response.status_code != HTTPStatus.NOT_FOUND, (
+            f'Страница `{url}` не найдена, проверьте этот адрес в *urls.py*.'
+        )
+    
+    def test_auth_with_invalid_data(self, client):
+        """Тест аутентификации с некорректными данными."""
+        url = '/api/v1/api-token-auth/'
+        response = client.post(url, data={})
+        
+        assert response.status_code == HTTPStatus.BAD_REQUEST, (
+            f'Проверьте, что POST-запрос к `{url}` с некорректными данными '
+            'возвращает ответ со статусом 400.'
+        )
